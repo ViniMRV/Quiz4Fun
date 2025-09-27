79999217fe63c9519086d2eadf136c6a2de2c861
Flip-Q <163772417+Flip-Q@users.noreply.github.com>
2025-09-27 16:36:19 +0000

    Pagina inicial e front

--- Backend/Quiz4FunProject/Quiz4FunProject/urls.py
+++ Backend/Quiz4FunProject/Quiz4FunProject/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 """
 
 
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 app_name = "users"
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("Users.urls")),
     path('quizzes/', include(('Quizzes.urls', 'quizzes'), namespace='quizzes')),
